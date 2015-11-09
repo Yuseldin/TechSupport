0fe36c365acdc0100eddf7d72030d4d5b2e92319
luguipe <lguilhermepperez@hotmail.com>
2015-11-09 14:37:41 +1000

    AddNewMember

--- TechSupport/Admin/AddNewMember.aspx.cs
+++ TechSupport/Admin/AddNewMember.aspx.cs
@@ -9,18 +9,29 @@ namespace TechSupport.Admin
 {
     public partial class AddNewMember : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            
-        }
+        
 
         protected void DropDownListSelectMember_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (DropDownListSelectMember.SelectedIndex.Equals(0))
+            if (DropDownListSelectMember.SelectedIndex.Equals(""))
             {
-                txtBoxAddName.Enabled = true;
-                txtBoxAddPhone.Enabled = true;
+                txtBoxAddName.Visible = false;
+                txtBoxAddPhone.Visible = false;
             }
+            else if (DropDownListSelectMember.SelectedIndex.Equals("Add Technician"))
+            {
+                txtBoxAddName.Visible = true;
+                txtBoxAddPhone.Visible = true;
+            }
+            else
+            { 
+                
+            }
+        }
+
+        protected void ContinueButton_Click(object sender, EventArgs e)
+        {
+            
         }
 
         
