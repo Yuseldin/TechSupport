9083c34c038b24e450bcf146ee0e710e766c7fb9
luguipe <lguilhermepperez@hotmail.com>
2015-11-23 10:50:23 +1000

    addtech

--- TechSupport/Admin/AddTechnician.aspx.cs
+++ TechSupport/Admin/AddTechnician.aspx.cs
@@ -20,7 +20,7 @@ namespace TechSupport.Admin
             int UserName = Convert.ToInt32(DetailsView1.Rows[0].Cells[1].Text.ToString());
 
             // increment the user name 
-            UserName = UserName + 1;
+            UserName++;
 
             //fill up the read only txtBox UserName
             CreateUserWizard1.UserName = UserName.ToString();
@@ -36,33 +36,75 @@ namespace TechSupport.Admin
             string Email = ((TextBox)CreateUserWizard1.CreateUserStep.ContentTemplateContainer.FindControl("Email")).Text;
 
             string conn = ConfigurationManager.ConnectionStrings["TechSupportConnectionString"].ConnectionString;
-            string insertTech = "INSERT INTO Technicians(Name, Email, Phone, TypeID) VALUES (@Name, @Email, @Phone, @TypeID)";
-
+            string insertTech = "INSERT INTO Technicians(Name, Email, Phone, TypeID) VALUES (@Name, @Email, @Phone, @TypeID)";            
 
             using (SqlConnection myConnection = new SqlConnection(conn))
             {
                 myConnection.Open();
 
                 SqlCommand command = new SqlCommand(insertTech,myConnection);
-
+                
+                //Inserting into user roles
                 command.Parameters.AddWithValue("@Name", Name);
                 command.Parameters.AddWithValue("@Email", Email);
                 command.Parameters.AddWithValue("@Phone", Phone);
+               
                 if (dropDown == "Support Officer Level 1")
                 {
                     command.Parameters.AddWithValue("@TypeID", 1);
                 }
                 else
                 {
-                    command.Parameters.AddWithValue("@TypeID", 2);
+                    command.Parameters.AddWithValue("@TypeID", 2);                    
                 }
 
-                command.ExecuteNonQuery();
+                command.ExecuteNonQuery(); //Executes the command to insert into TechSupport Database
 
-                myConnection.Close();
+                string conn2 = ConfigurationManager.ConnectionStrings["ASPNETConnectionString"].ConnectionString;
                 
-            }
 
+                using (SqlConnection myConn2 = new SqlConnection(conn2))
+                {
+                    myConn2.Open();
+
+                    string SelectUser = "SELECT UserId FROM aspnet_Users WHERE UserName = @username";
+                    string SelectRole = "SELECT RoleId FROM aspnet_Roles WHERE (RoleName = 'Support Officer Level 1')";
+                    string SelectRole2 = "SELECT RoleId FROM aspnet_Roles WHERE (RoleName = 'Technician Level 2')";
+
+                    //Retrieves the ID's
+                    SqlCommand cmdSelectUser = new SqlCommand(SelectUser, myConn2);
+                    SqlCommand cmdSelectRole = new SqlCommand(SelectRole, myConn2);
+                    SqlCommand cmdSelectRole2 = new SqlCommand(SelectRole2, myConn2);
+
+                    cmdSelectUser.Parameters.AddWithValue("@username", userName);
+
+                    string userID = cmdSelectUser.ExecuteNonQuery().ToString();
+                    string supportID = cmdSelectRole.ExecuteNonQuery().ToString();
+                    string techID = cmdSelectRole2.ExecuteNonQuery().ToString();
+
+                    if (dropDown == "Support Officer Level 1")
+                    {
+                        string insertRole = "INSERT INTO aspnet_UsersInRoles (UserID, RoleID) VALUES (@UserID, @SupportID)";
+                        SqlCommand insertSupport = new SqlCommand(insertRole, myConn2);
+                        insertSupport.Parameters.AddWithValue("@UserID", userID);
+                        insertSupport.Parameters.AddWithValue("@SupportID", supportID);
+
+                        insertSupport.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        string insertRole = "INSERT INTO aspnet_UsersInRoles (UserID, RoleID) VALUES (@UserID, @TechID)";
+                        SqlCommand insertTechnician = new SqlCommand(insertRole, myConn2);
+                        insertTechnician.Parameters.AddWithValue("@UserID", userID);
+                        insertTechnician.Parameters.AddWithValue("@TechID", techID);
+
+                        insertTechnician.ExecuteNonQuery();
+                    }
+
+                    myConnection.Close();
+                    myConn2.Close();
+                }              
+            }
         }
 
         protected void GetLastID_TextChanged(object sender, EventArgs e)
